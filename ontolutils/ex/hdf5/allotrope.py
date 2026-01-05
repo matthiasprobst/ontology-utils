4d90058b5cb0d32f0ef15aa1adf8e41982174b8c
MatthiasProbst <matth.probst@gmail.com>
2026-01-05 08:03:58 +0100

    add allotrope classes

--- ontolutils/ex/hdf5/allotrope.py
+++ ontolutils/ex/hdf5/allotrope.py
@@ -1,10 +1,11 @@
-from typing import Optional, List, Tuple, Any
+from typing import Optional, List, Tuple, Any, Union
 
 from pydantic import Field, field_validator
 from pydantic.functional_validators import WrapValidator
 from typing_extensions import Annotated
 
 from ontolutils import Thing, namespaces, urirefs
+from ontolutils.typing import AnyIriOf
 
 
 def is_internal_hdf5_path(path: str, handler):
@@ -27,11 +28,50 @@ _NS = "http://purl.allotrope.org/ontologies/hdf5/1.8#"
 
 
 @namespaces(hdf5=_NS)
+@urirefs(NamedObject='hdf5:NamedObject')
+class NamedObject(Thing):
+    """NamedObject"""
+
+
+@namespaces(hdf5=_NS)
+@urirefs(Dataspace='hdf5:Dataspace',
+         dimension='hdf5:dimension',
+         rank='hdf5:rank',
+         )
+class Dataspace(Thing):
+    """A dataset dataspace describes the dimensionality of the dataset. The dimensions of a dataset can be fixed (unchanging), or they may be unlimited, which means that they are extendible (i.e. they can grow larger). [Allotrope]"""
+    dimension: Optional[Union[int, List[int]]] = Field(default=None)
+    rank: Optional[int] = Field(default=None, ge=0, le=32)
+
+
+@namespaces(hdf5=_NS)
+@urirefs(StorageLayout='hdf5:StorageLayout')
+class StorageLayout(Thing):
+    """StorageLayout"""
+
+
+@namespaces(hdf5=_NS)
 @urirefs(Dataset='hdf5:Dataset',
-         name='hdf5:name')
+         name='hdf5:name',
+         dataspace='hdf5:dataspace',
+         layout='hdf5:layout',
+         rank='hdf5:rank',
+         )
 class Dataset(Thing):
-    """Dataset"""
+    """A multi-dimensional array of data elements, together with supporting metadata. [Allotrope]"""
     name: HDF5Path
+    dataspace: Optional[AnyIriOf[Dataspace]] = Field(
+        default=None)  # owl:qualifiedCardinality "1"^^xsd:nonNegativeInteger
+    layout: Optional[AnyIriOf[StorageLayout]] = Field(
+        default=None)  # owl:qualifiedCardinality "1"^^xsd:nonNegativeInteger
+    rank: Optional[int] = Field(default=None, ge=0, le=32)  # owl:qualifiedCardinality "1"^^xsd:nonNegativeInteger
+
+    @field_validator("name", mode="before")
+    @classmethod
+    def _validate_name(cls, name):
+        if not name.startswith('/'):
+            raise ValueError("Dataset name must start with '/'")
+        return name
 
 
 @namespaces(hdf5=_NS)
